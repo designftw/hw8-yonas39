1d1f6f0e4b3313bbbf7b1fea3c03b21d5ab1525a
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-04-12 11:31:26 -0400

    Bookmarklet

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,1 +1,83 @@
-/* put code here */
+javascript: (function () {
+  document.body.style.paddingTop = "20px";
+  var removedElements = [];
+  var removalActive = true;
+  var currentHighlightedElement;
+
+  // Create button at the top corner of the page
+  function createButton(text, callback) {
+    var btn = document.createElement("button");
+    btn.innerHTML = text;
+    btn.style.position = "fixed";
+    btn.style.top = "10px";
+    btn.style.zIndex = "999999";
+    btn.style.color = "Blue";
+    btn.style.backgroundColor = "orange";
+    btn.style.border = "1px solid black";
+    btn.style.padding = "5px";
+    btn.style.marginRight = "10px";
+    btn.onclick = callback;
+    document.body.appendChild(btn);
+    return btn;
+  }
+
+  // undo call back function
+  var undoButton = createButton("Undo", function () {
+    if (removedElements.length) {
+      var lastElement = removedElements.pop();
+      lastElement.parent.insertBefore(
+        lastElement.node,
+        lastElement.nextSibling
+      );
+    }
+  });
+
+  // End button remove all buttons and mouseover effect
+  var endButton = createButton("End", function () {
+    removalActive = false;
+    undoButton.remove();
+    endButton.remove();
+    document.removeEventListener("mousemove", highlightElement);
+  });
+
+  endButton.style.left = "calc(10px + " + undoButton.offsetWidth + "px)";
+
+  function highlightElement(event) {
+    if (currentHighlightedElement) {
+      currentHighlightedElement.style.outline = "";
+    }
+    var element = document.elementFromPoint(event.clientX, event.clientY);
+    if (element) {
+      currentHighlightedElement = element;
+      currentHighlightedElement.style.outline = "2px solid red";
+    }
+  }
+
+  document.addEventListener("mousemove", highlightElement);
+
+  document.addEventListener("click", function (event) {
+    if (!removalActive) return;
+    event.preventDefault();
+    event.stopPropagation();
+    if (currentHighlightedElement) {
+      var removedElement = {
+        node: currentHighlightedElement,
+        parent: currentHighlightedElement.parentNode,
+        nextSibling: currentHighlightedElement.nextSibling,
+      };
+      removedElements.push(removedElement);
+      currentHighlightedElement.remove();
+      currentHighlightedElement = null;
+    }
+  });
+
+  // when Escape key is pressed it exists the bokmarklet mode
+  document.addEventListener("keyup", function (event) {
+    if (event.key === "Escape") {
+      removalActive = false;
+      undoButton.remove();
+      endButton.remove();
+      document.removeEventListener("mousemove", highlightElement);
+    }
+  });
+})();
